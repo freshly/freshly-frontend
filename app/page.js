c2046ca45659d7700d4d05576fb85f44e8f3105d
Alex Farouz <alexfarouz@gmail.com>
2025-03-20 14:44:47 -0400

    fix classname issue

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import Hero from "./components/LandingPage/Hero";
 export default function Home() {
   return (
     <div
-      class="absolute inset-0 -z-10 h-full w-full bg-white 
-    bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] 
-    bg-[size:2rem_3rem]"
+      className="absolute inset-0 -z-10 h-full w-full bg-white 
+      bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] 
+      bg-[size:2rem_3rem]"
     >
       <Hero />
     </div>
